e53ed344947d305ede5f849493c7716493c0550e
Manasa Jagarlapudi <manasajagarlapudi@Manasas-MacBook-Air.local>
2023-11-15 16:21:57 +1300

    built api for carValue.

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { getCarValue } from "./carValue.js";
+const server = express();
+
+server.get("/:model/:year", (req, res) => {
+  const model = req.params.model;
+  const year = parseInt(req.params.year);
+
+  const value = getCarValue(model, year);
+  const result = {
+    model,
+    year,
+    value: `$${value}`,
+  };
+  res.send(result);
+});
+
+server.listen(8000, () => {
+  console.log("sever started and listening on port 8000");
+});
